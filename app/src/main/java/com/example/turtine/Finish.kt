ef532b8ddaae911089a2252a4af1083801d2f980
minji74 <wedfvb76@naver.com>
2023-01-26 20:07:02 +0900

    34

---
+++ app/src/main/java/com/example/turtine/Finish.kt
@@ -0,0 +1,14 @@
+package com.example.turtine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Finish : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+}
